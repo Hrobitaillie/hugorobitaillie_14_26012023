81cdcf16870f6b286a324f71a84feaa5cf1275e5
Hugo Robitaillie <hugo@la-ligne-web.com>
2023-02-09 11:26:14 +0100

    updte rooting using HashRouter

--- hrnet_v2/src/App.jsx
+++ hrnet_v2/src/App.jsx
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Layout/Header";
 import Layout from "./components/Layout/Layout";
 import NewEmployee from "./pages/NewEmployee";
@@ -11,14 +11,14 @@ export default function App() {
 
   return (
     <Fragment>
-      <BrowserRouter basename={routeBasename}>
+      <HashRouter basename={routeBasename}>
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<NewEmployee />} />
             <Route path="/team" element={<Team />} />
           </Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </Fragment>
   );
 }
